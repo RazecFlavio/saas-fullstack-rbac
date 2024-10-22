4782eebcfea40901477ce3a461606866a8e52de7
RazecFlavio <r4zecflavio@gmail.com>
2024-10-22 07:33:00 -0300

    Otimizando a tipagem das subjects

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -1,12 +1,12 @@
 import { AbilityBuilder } from "@casl/ability"
 import { AppAbility } from "."
 import { User } from "./models/user"
+import { Role } from "./role"
 
 type PermissionsByRole = (user: User, builder: AbilityBuilder<AppAbility>) => void
 
-type Role = 'ADMIN' | 'MEMBER'
-
 export const permissions: Record<Role, PermissionsByRole> = {
     ADMIN(_, builder) { },
-    MEMBER(_, builder) { }
+    MEMBER(_, builder) { },
+    BILLING() { }
 }
