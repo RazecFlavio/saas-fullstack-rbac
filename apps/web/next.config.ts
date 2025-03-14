1067373f3a0419338c1995b9378f163044c69ee7
RazecFlavio <r4zecflavio@gmail.com>
2025-03-14 07:58:59 -0300

    Listando membros de uma organization

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import { hostname } from "os";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      { hostname: 'github.com' },
+      { hostname: 'avatars.githubusercontent.com' }
+    ]
+  }
 };
 
 export default nextConfig;
---
+++ apps/web/src/http/get-members.ts
@@ -0,0 +1,20 @@
+import { Role } from "@saas/auth";
+import { api } from "./api-client"
+
+interface GetMembersResponse {
+    members: {
+        name: string | null;
+        id: string;
+        avatarUrl: string | null;
+        role: Role;
+        userId: string;
+        email: string;
+    }[]
+}
+
+export async function getMembers(org: string) {
+    //await new Promise(resolve => setTimeout(resolve, 2000))
+
+    const result = await api.get(`organizations/${org}/members`).json<GetMembersResponse>()
+    return result
+}
